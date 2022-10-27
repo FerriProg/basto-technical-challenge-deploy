5e4243b74895eacbf1d71f15f44ae237d1584f4f
unknown <cgferreiro1985@gmail.com>
2022-10-27 11:30:51 -0300

    deploy2

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
 
 axios.defaults.baseURL =
   'https://basto-tc-ferriprog.herokuapp.com' || 'http://localhost:3001';
